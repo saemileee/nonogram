2e04950ca6e0deae0fd0af03e52b2689233697fa
saemileee <saemilee01@gmail.com>
2023-03-12 23:31:35 +0900

    숫자 생성 로직 수정

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ for (let x = 1; x <= boardSize; x++) {
   xTaskContainerEl.appendChild(xTaskBoxEl);
 
   const yTaskBox = document.createElement("tr");
-  yTaskBox.setAttribute("class", `x${x} task-box`);
+  yTaskBox.setAttribute("class", `y${x} task-box`);
   yTaskContainerEl.appendChild(yTaskBox);
 
   //로우 만들기
@@ -32,11 +32,55 @@ for (let x = 1; x <= boardSize; x++) {
       //클릭한 셀의 좌표
       const thisX = parseInt(e.target.getAttribute("data-x"));
       const thisY = parseInt(e.target.getAttribute("data-y"));
+
+      //토글로 cell 배열 변경
+      cells.map((cell) =>
+        cell.x === thisX && cell.y === thisY ? (cell.is = !cell.is) : null
+      );
+      //토글로 클래스 변경(셀 채우기)
+      e.target.classList.toggle("true");
+      puzzleSetting(thisX, thisY);
     });
   }
 }
 
 // 문제 세팅
+
+function puzzleSetting(x, y) {
+  const xCells = cells.filter((cell) => cell.x === x);
+
+  let xTasks = [];
+  let xTaskCount = 0;
+
+  const yCells = cells.filter((cell) => cell.y === y);
+  let yTasks = [];
+  let yTaskCount = 0;
+
+  //클릭한 x,y축의 배열을 순회해서 is값 확인하기
+  for (let i = 0; i < boardSize; i++) {
+    //is값이 true면 1을 더하기, 마지막 배열에서는 task배열에 넣기
+    if (xCells[i].is === true) {
+      xTaskCount += 1;
+      i === boardSize - 1 ? xTasks.push(xTaskCount) : null;
+    } else {
+      //is 값이 false이고, 카운팅 된 숫자가 0이상이면 배열에 넣기
+      xTaskCount && xTasks.push(xTaskCount);
+      xTaskCount = 0;
+    }
+
+    //is값이 true면 1을 더하기, 마지막 배열에서는 task배열에 넣기
+    if (yCells[i].is === true) {
+      yTaskCount += 1;
+      i === boardSize - 1 ? yTasks.push(yTaskCount) : null;
+    } else {
+      //is 값이 false면 카운팅 된 숫자가 0이상이면 배열에 넣기
+      yTaskCount && yTasks.push(yTaskCount);
+      yTaskCount = 0;
+    }
+  }
+  console.log(xTasks);
+  console.log(yTasks);
+}
 
 // function puzzleSetting() {
 //   for (let i = 0; i < boardSize; i++) {
@@ -64,8 +108,5 @@ for (let x = 1; x <= boardSize; x++) {
 //     }
 //   }
 // }
-
-//set btn
-const setBtn = document.getElementById("set-btn");
 
 // setBtn.addEventListener("click", puzzleSetting);
